8e0da7d3912c1022f83a341e846a620cbe85fea9
krishscreativecreations <159649895+krishscreativecreations@users.noreply.github.com>
2024-02-11 12:43:29 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,57 @@ function register(){
    email = document.getElementById('input-email').value
    password = document.getElementById('input-pass').value
 
+
+   if (validate_email(email) == false || validate_password(password) == false) {
+      return
+
+   auth.createUserWithEmailAndPassword(email, password)
+   .then(function() {
+      var user = auth.currentUser
+      var database_ref = database.ref()
+      var user_data = {
+         email : email
+         password : password
+         last-login : Date.now()
+      }
+
+      database_ref.child('users/' + user.uid).set(user_data)
+      alert('User Created!)
+   })
+   .catch(function(error) {
+      var error_code = error.code
+      var error_message =  error.message
+
+      alert(error_message)
+   })
+
+function validate_email(email) {
+   expression = /^[^@]+@\w+(\.\w+)+\w$/
+   if (expression.test(email)==true) {
+      return true
+   } else{
+      return false
+   }
+}
+
+function validate_password(password) {
+   if (password < 6) {
+      return false
+   }  else {
+      return true
+   }
+}
+
+function validate_field(field) {
+   if (field == null) {
+      return false
+   }
+
+   if (field.length <= 0) {
+      return false
+   }  else {
+      return true
+   }
+}
+      
+
